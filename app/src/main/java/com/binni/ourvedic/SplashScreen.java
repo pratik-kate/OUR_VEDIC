48da1b727420fe4e8288660f3adaf202635721e0
pratikkate3 <pratikrkate@gmail.com>
2020-11-14 01:17:37 +0530

    splashScreen

---
+++ app/src/main/java/com/binni/ourvedic/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.binni.ourvedic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        },3000);
+
+    }
+}
