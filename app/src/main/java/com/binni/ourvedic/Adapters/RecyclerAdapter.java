0f1ba59cca7d37bbf5f3df0c178a92bd817d4561
Binni Goel <binni1108@gmail.com>
2020-11-14 14:58:10 +0530

    Added Different Activity with Different Religions

--- app/src/main/java/com/binni/ourvedic/Adapters/RecyclerAdapter.java
+++ app/src/main/java/com/binni/ourvedic/Adapters/RecyclerAdapter.java
@@ -1,17 +1,25 @@
 package com.binni.ourvedic.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.binni.ourvedic.BuddhActivity;
+import com.binni.ourvedic.ChristianActivity;
+import com.binni.ourvedic.HinduActivity;
+import com.binni.ourvedic.IslamActivity;
+import com.binni.ourvedic.JainActivity;
 import com.binni.ourvedic.Models.RecyclerModel;
 import com.binni.ourvedic.R;
+import com.binni.ourvedic.SikhActivity;
 
 import java.util.ArrayList;
 
@@ -49,6 +57,59 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.viewHo
           holder.image.setImageResource(model.getImage());
           holder.text.setText(model.getText());
 
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Intent intent;
+                switch (position){
+                    case 0:
+
+                        intent =  new Intent(context, HinduActivity.class);
+                        Toast.makeText(context, "Hindu Toast ", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case 1:
+
+                        intent =  new Intent(context, IslamActivity.class);
+                        Toast.makeText(context, "Islam Toast ", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case 2:
+
+                        intent =  new Intent(context, ChristianActivity.class);
+                        Toast.makeText(context, "Christian Toast ", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case 3:
+
+                        intent =  new Intent(context, SikhActivity.class);
+                        Toast.makeText(context, "Sikh Toast ", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case 4:
+
+                        intent =  new Intent(context, BuddhActivity.class);
+                        Toast.makeText(context, "Buddh Toast ", Toast.LENGTH_LONG).show();
+                        break;
+
+                    case 5:
+                        intent =  new Intent(context, JainActivity.class);
+                        Toast.makeText(context, "Jain Toast ", Toast.LENGTH_LONG).show();
+                        break;
+
+
+                    default:
+                        intent =  new Intent(context, HinduActivity.class);
+                        Toast.makeText(context, "Hindu Toast ", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                context.startActivity(intent);
+                // Here You Do Your Click Magic
+            }
+        });
+
+
 
 
     }
@@ -64,12 +125,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.viewHo
 
         ImageView image;
             TextView text;
+        View mView;
+        private final Context context;
            public viewHolder(@NonNull View itemView) {
                super(itemView);
+               context = itemView.getContext();
 
                image = itemView.findViewById(R.id.image);
                text = itemView.findViewById(R.id.text);
-
+               mView = itemView;
            }
        }
 }
--- app/src/main/java/com/binni/ourvedic/fragments/ShopFragment.java
+++ app/src/main/java/com/binni/ourvedic/fragments/ShopFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -46,6 +48,9 @@ public class ShopFragment extends Fragment {
         GridLayoutManager layout = new GridLayoutManager(getContext(),2);
         list.setLayoutManager(layout);
 
+
         return view;
     }
+
+
 }
